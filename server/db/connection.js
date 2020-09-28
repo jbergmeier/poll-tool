d37f65403fec4b21d29414bcd38f3deeb40efed6
Joern Bergmeier <jbergmeier@Joerns-MacBook-Pro.local>
2020-09-28 20:11:48 +0200

    changed connection string

--- server/db/connection.js
+++ server/db/connection.js
@@ -1,6 +1,10 @@
 const mongoose = require("mongoose")
 require("dotenv").config()
-mongoose.connect('mongodb+srv://',process.env.DB_USER,':',process.env.DB_PASSWORD,'@cluster0.rlosr.mongodb.net/',process.env.DB_DATABASE,'?retryWrites=true&w=majority&ssl=true', { useNewUrlParser: true, useUnifiedTopology: true});
+const db_user = process.env.DB_USER
+const db_password = process.env.DB_PASSWORD
+const db_db = process.env.DB_DATABASE
+
+mongoose.connect(`mongodb+srv://${db_user}:${db_password}@cluster0.rlosr.mongodb.net/${db_db}?retryWrites=true&w=majority&ssl=true`, { useNewUrlParser: true, useUnifiedTopology: true});
 
 const db = mongoose.connection;
 db.on('error', console.error.bind(console, 'connection error:'));
